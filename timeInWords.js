3019451a1453cc041e13fe5f8be370724a631d2f
Shreyas Vankayalapati <59900350+svankaya2001@users.noreply.github.com>
2021-01-28 13:51:24 -0800

    Update timeInWords.js

--- timeInWords.js
+++ timeInWords.js
@@ -4,7 +4,7 @@ function timeInWords(h, m) {
         13: 'thirteen', 14: 'fourteen', 15: 'quarter', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen', 20: 'twenty', 21: 'twenty one', 22: 'twenty two', 23: 'twenty three', 24: 'twenty four', 25: 'twenty five', 26: 'twenty six', 27 : 'twenty seven', 28: 'twenty eight', 29: 'twenty nine', 30: 'half'  }
     
     let output_str = '';
-    if(m===0)output_str = mins_dict[h] + ' '+hours_dict[0]
+    if(m===0)output_str = mins_dict[h] + ' '+mins_dict[0]
     else if(m>30)output_str = mins_dict[60-m]  + (60-m===1?' minute ': ((60-m)%15===0?' ': ' minutes ')) + 'to '+ mins_dict[h+1];
     else{
         output_str = mins_dict[m] + (m===1?' minute ': (m%15===0?' ': ' minutes ')) + 'past '+ mins_dict[h];
